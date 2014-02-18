2fcad5a6786dd384dd43b4fdef97a63265a16fb4
tasmail <tas.gma@gmail.com>
2014-02-18 20:03:34 +0100

    Routes for editing.

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,4 @@ resources :goods
 match ':project_id/goods'  => "goods#index"
 match ':project_id/goods/new'  => "goods#new"
 match ':project_id/goods/:id'  => "goods#show"
+match ':project_id/goods/:id/edit'  => "goods#edit"
